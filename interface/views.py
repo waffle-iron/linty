c7fd0ef9cdb5421ab45735e1a3538700493caddd
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-25 00:38:17 -0700

    Implement status updates

--- interface/views.py
+++ interface/views.py
@@ -5,6 +5,7 @@ import subprocess
 
 import shutil
 
+import requests
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -21,11 +22,10 @@ class ResultDetailView(DetailView):
 def WebhookView(request):
     body = json.loads(request.body)
 
-    # TODO: set pending status
-
     # TODO: get credentials from User
     username = 'birkholz'
     password = '************'
+    auth = (username, password)
 
     # get necessary vars
     repo_name = body['repository']['name']
@@ -33,6 +33,19 @@ def WebhookView(request):
     clone_url = clone_url.replace('github.com', '%s:%s@github.com' % (username, password))
     branch = body['ref'].replace('refs/heads/', '')
     combo_name = '/'.join([repo_name, branch])
+    sha = body['head_commit']['id']
+    status_url = body['repository']['statuses_url'].replace('{sha}', sha)
+
+    def publish_status(state, description, target_url=None):
+        data = {
+            'state': state,
+            'description': description,
+            'target_url': target_url,
+            'context': 'pepsi'
+        }
+        requests.post(status_url, json=data, auth=auth)
+
+    publish_status('pending', 'Linting your code...')
 
     # download repo
     if not os.path.exists('tmp'):
@@ -53,13 +66,13 @@ def WebhookView(request):
         output = e.output
 
     if not output:
-        pass
-        # TODO: set success status
+        publish_status('success', 'Your code conforms to pep8.')
     else:
         output = output.replace(directory, '')
         # save record
         result = Result.objects.create(text=output)
-        url = reverse('result_detail', kwargs={'pk': result.id})
-        # TODO: set error status
+        path = reverse('result_detail', kwargs={'pk': result.id})
+        url = request.build_absolute_uri(path)
+        publish_status('error', 'Your code has pep8 violations.', target_url=url)
 
     return HttpResponse(status=204)

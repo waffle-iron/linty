bfa62f5f8a0aa647d2e7f9eacc9fb357980b532d
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-25 21:56:04 -0700

    Enable the Django admin

--- interface/admin.py
+++ interface/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from interface.models import Result
+
+
+class ResultAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Result
--- linty/urls.py
+++ linty/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
     url('', include('interface.urls')),
-    url('', include('social.apps.django_app.urls', namespace='social'))
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^admin/', admin.site.urls),
 ]

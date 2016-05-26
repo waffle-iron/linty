6cb7ced123224925df20643b14de263dcbbcfed9
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-26 16:39:22 -0700

    Add build list view

--- interface/urls.py
+++ interface/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from interface import views
 
 urlpatterns = [
-    url(r'^build/(?P<pk>[0-9]+)$', views.BuildDetailView.as_view(), name='build_detail'),
-    url(r'^repos$', views.RepoListView.as_view(), name='repo_list'),
     url(r'^add/(?P<full_name>.*)$', views.ProcessRepo, name='process_repo'),
+    url(r'^repos$', views.RepoListView.as_view(), name='repo_list'),
+    url(r'^repo/(?P<pk>[0-9]+)/builds$', views.BuildListView.as_view(), name='build_list'),
     url(r'^repo/(?P<pk>[0-9]+)/delete$', views.RepoDeleteView.as_view(), name='delete_repo'),
+    url(r'^build/(?P<pk>[0-9]+)$', views.BuildDetailView.as_view(), name='build_detail'),
     url(r'^webhook$', views.WebhookView, name='webhook')
 ]

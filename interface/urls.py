e97392daa3e3b5d8686de4fe7a268612587fc845
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-26 01:51:20 -0700

    Change repo delete url

--- interface/urls.py
+++ interface/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^build/(?P<pk>[0-9]+)$', views.BuildDetailView.as_view(), name='build_detail'),
     url(r'^repos$', views.RepoListView.as_view(), name='repo_list'),
     url(r'^add/(?P<full_name>.*)$', views.ProcessRepo, name='process_repo'),
-    url(r'^repo/(?P<pk>[0-9]+)$', views.RepoDeleteView.as_view(), name='delete_repo'),
+    url(r'^repo/(?P<pk>[0-9]+)/delete$', views.RepoDeleteView.as_view(), name='delete_repo'),
     url(r'^webhook$', views.WebhookView, name='webhook')
 ]

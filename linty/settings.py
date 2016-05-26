886f4a052b8decf08d2e036237c793c203ca3071
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-25 21:48:17 -0700

    Set static root

--- linty/settings.py
+++ linty/settings.py
@@ -129,5 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'email']
